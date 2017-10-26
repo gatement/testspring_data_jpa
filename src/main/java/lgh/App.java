a9f872f1d3c27e3f1490ea247419f90070c56fe3
JohnsonLau <gatement@gmail.com>
2017-10-26 18:31:11 +0800

    go

--- src/main/java/lgh/App.java
+++ src/main/java/lgh/App.java
@@ -5,7 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 
 @SpringBootApplication
 public class App {
@@ -50,4 +55,29 @@ public class App {
 			log.info("");
 		};
 	}
+
+    // the setting "spring.datasource.druid.filters=stat" replace the following code
+	/*
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+        //reg.addInitParameter("allow", "127.0.0.1"); //白名单
+        //reg.addInitParameter("deny",""); //黑名单
+        reg.addInitParameter("loginUsername", "admin");
+        reg.addInitParameter("loginPassword", "admin");
+        reg.addInitParameter("resetEnable", "false");//禁用HTML页面上的“Reset All”功能
+        return reg;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+    */
 }
